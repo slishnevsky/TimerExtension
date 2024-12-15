599c49db72db1a15a33002dae3009d8887dd78c8
Slava Lishnevsky <slishnevsky@gmail.com>
2024-12-15 06:40:01 -0500

    initial commit

---
+++ background.js
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.action.setBadgeText({ text: '' });
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: 'images/time-management.png',
+    title: 'Сработал Таймер',
+    message: "Ты ещё помнишь для чего этот таймер был установлен ?!",
+    buttons: [{ title: 'Продолжить' }],
+    priority: 0
+  });
+});
+
+chrome.notifications.onButtonClicked.addListener(async () => {
+  const item = await chrome.storage.sync.get(['minutes']);
+  chrome.action.setBadgeText({ text: 'ON' });
+  chrome.alarms.create({ delayInMinutes: item.minutes });
+});
+
+
